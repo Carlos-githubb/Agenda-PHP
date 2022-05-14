9ffecfeacee1d5d3a552d7da9047ae728aed49c8
Carlos Salinas <kar.salinas16@gmail.com>
2022-05-14 20:54:24 +0300

    my first commit in the agenda php project

---
+++ public/js/agenda.js
@@ -0,0 +1,20 @@
+
+<script>
+
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('agenda');
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    locale: "es",
+
+    headerToolbar: {
+        left: 'prev, next, today',
+        center: 'title',
+        right: 'dayGridMonth, timeGridWeek, listWeek'
+     
+    }
+  });
+  calendar.render();
+});
+
+</script>
---
+++ resources/views/event/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div id= "agenda">
+        Calendario
+    </div>
+</div>
+
+@endsection
